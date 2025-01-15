0d31c2221c3cd1fafce55a4828adc6c954aa3ee4
andrewwatford <84861715+andrewwatford@users.noreply.github.com>
2025-01-15 16:51:48 -0500

    using more data for wind

--- examples/aw/3d_models/import_wind_data.jl
+++ examples/aw/3d_models/import_wind_data.jl
@@ -1,4 +1,4 @@
-using Printf, CairoMakie, NCDatasets, Interpolations, GLMakie, Impute
+using Printf, CairoMakie, NCDatasets, Interpolations, GLMakie, Impute, Dates, Statistics
 using Oceananigans.Units
 
 const year = years = 365days;
@@ -10,14 +10,24 @@ lat_min, lat_max = 24.625, 55.375
 lat_nodes = Vector(lat_min:0.25:lat_max)
 t_nodes = 0:1:12
 
-ds = NCDataset("./data/copernicus/wind/cmems_obs-wind_glo_phy_my_l4_P1M_1736543077049.nc");
+ds = NCDataset("./data/copernicus/wind/new.nc");
 lat_min_idx, lat_max_idx = findfirst(ds["latitude"] .== lat_min), findfirst(ds["latitude"] .== lat_max)
 lon_min_idx, lon_max_idx = findfirst(ds["longitude"] .== lon_min), findfirst(ds["longitude"] .== lon_max)
 
+data_by_month = [[],[],[],[],[],[],[],[],[],[],[],[]];
 wind_dat = convert(Array{Union{Missing, Float64}}, ds["eastward_stress"][lon_min_idx:lon_max_idx, lat_min_idx:lat_max_idx, :])
 for t = 1:(size(wind_dat)[end])
-    wind_dat[:,:,t] = Impute.interp(wind_dat[:,:,t]) |> Impute.locf() |> Impute.nocb()
+    wind_dat[:,:,t] = Impute.interp(wind_dat[:,:,t]) |> Impute.locf() |> Impute.nocb();
+    push!(data_by_month[Dates.month(ds["time"][t])], wind_dat[:,:,t]);
 end
+
+mean_data_by_month = []
+for t = 1:12
+    push!(mean_data_by_month, mean(data_by_month[t]));
+end
+push!(mean_data_by_month, mean_data_by_month[1]);
+wind_dat = stack(mean_data_by_month);
+
 wind_itp = interpolate((lon_nodes, lat_nodes, t_nodes), wind_dat, Gridded(Linear()));
 τ_surface(x, y, t) = wind_itp(x, y, mod(t, year) / month);
 
